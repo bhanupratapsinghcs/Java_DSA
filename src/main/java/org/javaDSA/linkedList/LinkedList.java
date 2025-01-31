b9afd80ded60200ad94599e503ea3eab944cb5d3
Bhanu Pratap Singh <44972186+bhanupratapsinghcs@users.noreply.github.com>
2025-01-31 22:14:25 +0530

    Added removeLast, prepend, removeFirst, get and set method

--- src/main/java/org/javaDSA/linkedList/LinkedList.java
+++ src/main/java/org/javaDSA/linkedList/LinkedList.java
@@ -38,7 +38,7 @@ public class LinkedList {
     }
 
     // Add a new node at the end of the linked list
-    public void addNode(int data){
+    public void append(int data){
         Node newNode = new Node(data);
         if(head == null){
             head = newNode;
@@ -46,6 +46,77 @@ public class LinkedList {
             tail.next = newNode;
         }
         tail = newNode;
+        length++;
+    }
+
+    // Remove a node from the end of the linked list
+    public Node removeLast(){
+        if(head == null){
+            return null;
+        }
+        Node current = head;
+        Node previous = null;
+        while(current != tail){
+            previous = current;
+            current = current.next;
+        }
+        if(previous != null){
+            previous.next = null;
+            tail = previous;
+            length--;
+        }else{
+            head = null;
+            tail = null;
+            length = 0;
+        }
+        return current;
+    }
+
+    // Add a new node at the beginning of the linked list
+    public void prepend(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+
+    // Remove a node from the beginning of the linked list
+    public Node removeFirst(){
+        if(head == null){
+            return null;
+        }
+        Node removedNode = head;
+        head = head.next;
+        length--;
+        return removedNode;
+    }
+
+    // Get a node at a specific index
+    public Node get(int index){
+        if(index < 0 || index >= length){
+            return null;
+        }
+        Node current = head;
+        int count = 0;
+        while(count < index) {
+            current = current.next;
+            count++;
+        }
+        return current;
+    }
+
+    // set a node at a specific index
+    public void set(int index, int data){
+        Node current = get(index);
+        if(current != null){
+            current.data = data;
+        }
+        if(current == null)
+            System.out.println("Index out of bound");
     }
 
     // Display the linked list
--- src/main/java/org/javaDSA/linkedList/Main.java
+++ src/main/java/org/javaDSA/linkedList/Main.java
@@ -3,11 +3,41 @@ package org.javaDSA.linkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList linkList = new LinkedList();
-        linkList.addNode(1);
-        linkList.addNode(2);
-        linkList.addNode(3);
-        linkList.addNode(4);
-        linkList.addNode(5);
+        linkList.append(1);
+        linkList.append(2);
+        linkList.append(3);
+        linkList.append(4);
+        linkList.append(5);
+        linkList.prepend(6);
+        linkList.prepend(7);
+        linkList.prepend(8);
+        linkList.prepend(9);
+        System.out.println(linkList.get(0).data);
+        System.out.println(linkList.get(3).data);
+        System.out.println(linkList.get(4).data);
+        System.out.println(linkList.get(8).data);
+        linkList.set(5, 0);
+        linkList.set(9, 0);
+        linkList.printLinkedList();
+        System.out.println(linkList.removeLast().data + " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeLast().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeLast().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeLast().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeLast().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeFirst().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeFirst().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeFirst().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeFirst().data+ " " + linkList.getLength());
+        linkList.printLinkedList();
+        System.out.println(linkList.removeFirst()+ " " + linkList.getLength());
         linkList.printLinkedList();
     }
 }
