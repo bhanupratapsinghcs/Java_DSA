2105aff7b20390d31e7ceffc8b7d2fea65f3668a
Bhanu Pratap Singh <44972186+bhanupratapsinghcs@users.noreply.github.com>
2025-02-06 19:08:33 +0530

    insert, remove and reverse method added

--- src/main/java/org/javaDSA/linkedList/LinkedList.java
+++ src/main/java/org/javaDSA/linkedList/LinkedList.java
@@ -119,6 +119,60 @@ public class LinkedList {
             System.out.println("Index out of bound");
     }
 
+    // Insert a node at a specific index
+    public void insert(int index, int data){
+        if(index < 0 || index > length){
+            System.out.println("Index out of bound");
+            return;
+        }
+        if(index == 0){
+            prepend(data);
+            return;
+        }
+        if(index == length){
+            append(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node current = get(index - 1);
+        newNode.next = current.next;
+        current.next = newNode;
+        length++;
+    }
+
+    // Remove a node at a specific index
+    public Node remove(int index){
+        if(index < 0 || index >= length){
+            return null;
+        }
+        if(index == 0){
+            return removeFirst();
+        }
+        if(index == length - 1){
+            return removeLast();
+        }
+        Node current = get(index - 1);
+        Node removedNode = current.next;
+        current.next = removedNode.next;
+        length--;
+        return removedNode;
+    }
+
+    public void reverse(){
+        Node current = head;
+        Node previous = null;
+        Node next = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        tail = head;
+        head = previous;
+        System.out.println("LinkList reversed");
+    }
+
     // Display the linked list
     public void printLinkedList(){
         Node current = head;
