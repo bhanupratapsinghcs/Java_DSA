06bea5e3c1c0a20b7fb632cc99c86bd391844f6c
Bhanu Pratap Singh <44972186+bhanupratapsinghcs@users.noreply.github.com>
2025-02-08 18:48:56 +0530

    Added Stack

--- src/main/java/org/javaDSA/stack/Main.java
+++ src/main/java/org/javaDSA/stack/Main.java
@@ -1,2 +1,18 @@
-package org.javaDSA.stack;public class Main {
+package org.javaDSA.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.printStack();
+        System.out.println("-----");
+        System.out.println("Height -> " + stack.getHeight());
+        System.out.println("-----");
+        System.out.println("Pop -> " + stack.pop().data);
+        stack.printStack();
+    }
 }
--- src/main/java/org/javaDSA/stack/Stack.java
+++ src/main/java/org/javaDSA/stack/Stack.java
@@ -1,2 +1,56 @@
-package org.javaDSA.stack;public class Stack {
+package org.javaDSA.stack;
+
+public class Stack {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node top = null;
+    private int height = 0;
+
+    public Node getTop() {
+        return top;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+    public void printStack(){
+        if(top == null){
+            System.out.println("The stack is empty.");
+            return;
+        }
+        Node current = top;
+        while(current != null){
+            System.out.println("| " + current.data + " |");
+            current = current.next;
+        }
+    }
+
+    // Add a new node at the top of the stack
+    public void push(int data){
+        Node newNode = new Node(data);
+        if(top == null){
+            top = newNode;
+        }else{
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    // Remove top node from the stack
+    public Node pop(){
+        if(top == null){
+            return null;
+        }
+        Node current = top;
+        top = top.next;
+        height--;
+        return current;
+    }
 }
